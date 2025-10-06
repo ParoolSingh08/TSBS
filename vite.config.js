80600931d04514edd50cd9bc8a93dbfbfc83f7fb
Parool Singh <parool.singh@kryil.com>
2025-10-06 18:33:56 +0530

    Final production build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/T5B5/', // 👈 important for GitHub Pages
   plugins: [react()],
+  build: {
+    assetsInclude: ['**/*.otf'] // 👈 ensures .otf fonts are bundled
+  }
 })
